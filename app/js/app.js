7808b261a6cadf1689b4841a52759fe346411a93
Simon Lomax <simax.99.99@gmail.com>
2013-03-30 17:28:40 +0000

    Added gravatar directive and some work on UI

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 var controllers = angular.module('staffApp.controllers', []);
 var directives = angular.module('staffApp.directives', []);
 var filters = angular.module('staffApp.filters', []);
-var services = angular.module('staffApp.services', []);
+var services = angular.module('staffApp.services', ['md5', 'ui-gravatar']);
 var staffApp = angular.module('staffApp', ['staffApp.controllers', 'staffApp.filters', 'staffApp.services', 'staffApp.directives']);
 
 staffApp.config(['$routeProvider', function($routeProvider) {
@@ -16,16 +16,16 @@ services.factory('departmentsModel', function() {
         return [
             {'name' : 'Development',
                 'employees' : [
-                    {'firstname' : 'Dan', 'lastname' : 'Ormisher', 'email' : 'dan.ormisher@ekmsystems.co.uk'},
-                    {'firstname' : 'Nick', 'lastname' : 'Stansfield', 'email' : 'nick.stanfield@ekmsystems.co.uk'},
-                    {'firstname' : 'Simon', 'lastname' : 'Lomax', 'email' : 'simon.lomax@ekmsystems.co.uk'}
+                    {'firstname' : 'Dan', 'lastname' : 'Ormisher', 'email' : 'danielormisher@ekmsystems.co.uk'},
+                    {'firstname' : 'Nick', 'lastname' : 'Stansfield', 'email' : 'nickstansfield@ekmsystems.co.uk'},
+                    {'firstname' : 'Simon', 'lastname' : 'Lomax', 'email' : 'simonlomax@ekmsystems.co.uk'}
                 ]},
             {'name' : 'Design',
                 'employees' : [
-                    {'firstname' : 'David', 'lastname' : 'Sharpe', 'email' : 'david.sharpe@ekmsystems.co.uk'}]},
+                    {'firstname' : 'David', 'lastname' : 'Sharpe', 'email' : 'davidsharpe@ekmsystems.co.uk'}]},
             {'name' : 'Customer Services',
                 'employees' : [
-                    {'firstname' : 'Matthew', 'lastname' : 'Allingham', 'email' : 'matthew.allingham@ekmsystems.co.uk'}]},
+                    {'firstname' : 'Matthew', 'lastname' : 'Allingham', 'email' : 'matthewallingham@ekmsystems.co.uk'}]},
             {'name' : 'Sales'}
         ];
     };
