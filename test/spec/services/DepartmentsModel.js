d88bd923e87c06b4cb700f9cdfe7f1f8d58857c3
Simon Lomax <simax.99.99@gmail.com>
2013-05-16 20:18:17 +0100

    Attempting to fix tests and removed main.js etc

--- test/spec/services/DepartmentsModel.js
+++ test/spec/services/DepartmentsModel.js
@@ -3,16 +3,21 @@
 describe('Service: DepartmentsModel', function () {
 
   // load the service's module
-  beforeEach(module('StaffApp'));
+  beforeEach(module('StaffApp.services'));
 
   // instantiate service
-  var DepartmentsModel;
-  beforeEach(inject(function (_DepartmentsModel_) {
-    DepartmentsModel = _DepartmentsModel_;
+  var departmentsModel;
+    
+  beforeEach(inject(function (DepartmentsModel) {
+    departmentsModel = DepartmentsModel;
   }));
 
-  it('should do something', function () {
-    expect(DepartmentsModel).toBeDefined();
+  it('should be defined', function () {
+    expect(departmentsModel).toBeDefined();
+  });
+
+  it('should have method getDepartments()', function () {
+    expect(departmentsModel.getDepartments()).toBeDefined();
   });
 
 });
